7b615763406af4a7ead276e92e39dafbcf234060
fantasynitrogen <fantasynitrogen@gmail.com>
2019-02-22 10:55:55 +0300

    Revert: removed web/plugins/opennlp-dictionary-extractor

---
+++ web/plugins/opennlp-dictionary-extractor/src/main/java/org/visallo/opennlpDictionary/web/AdminDictionary.java
@@ -0,0 +1,51 @@
+package org.visallo.opennlpDictionary.web;
+
+import com.google.inject.Inject;
+//import com.v5analytics.webster.ParameterizedHandler;
+//import com.v5analytics.webster.annotations.Handle;
+import org.visallo.webster.*;
+import org.visallo.webster.annotations.Handle;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.visallo.core.config.Configuration;
+import org.visallo.core.model.user.UserRepository;
+import org.visallo.core.model.workspace.WorkspaceRepository;
+import org.visallo.core.simpleorm.SimpleOrmContextProvider;
+import org.visallo.core.user.User;
+import org.visallo.opennlpDictionary.model.DictionaryEntry;
+import org.visallo.opennlpDictionary.model.DictionaryEntryRepository;
+
+import org.visallo.core.simpleorm.SimpleOrmContextProvider;
+
+
+public class AdminDictionary implements ParameterizedHandler {
+    private DictionaryEntryRepository dictionaryEntryRepository;
+    private SimpleOrmContextProvider simpleOrmContextProvider;
+
+    @Inject
+    public AdminDictionary(
+            final DictionaryEntryRepository dictionaryEntryRepository,
+            final UserRepository userRepository,
+            final WorkspaceRepository workspaceRepository,
+            final Configuration configuration) {
+        this.dictionaryEntryRepository = dictionaryEntryRepository;
+    }
+
+    @Handle
+    public JSONObject handle(
+            User user
+    ) throws Exception {
+
+        //Iterable<DictionaryEntry> dictionary = dictionaryEntryRepository.findAll(user.getSimpleOrmContext());
+        Iterable<DictionaryEntry> dictionary = dictionaryEntryRepository.findAll(simpleOrmContextProvider.getContext(user));
+        JSONArray entries = new JSONArray();
+        JSONObject results = new JSONObject();
+        for (DictionaryEntry entry : dictionary) {
+            entries.put(entry.toJson());
+        }
+
+        results.put("entries", entries);
+
+        return results;
+    }
+}
